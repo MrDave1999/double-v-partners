09f86f9a77fda37228a8644369cabd3f844a6fcd
MrDave1999 <daveseva2010@hotmail.es>
2023-11-20 19:31:14 -0500

    chore: Add SimpleResults.FluentValidation package

--- backend/src/Extensions/ValidationResultExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Double.Partners.Extensions;
-
-public static class ValidationResultExtensions
-{
-    public static bool IsFailed(this ValidationResult result) => !result.IsValid;
-    public static IEnumerable<string> AsErrors(this ValidationResult result)
-        => result.Errors.Select(failure => failure.ErrorMessage);
-}
